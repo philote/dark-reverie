ac5966e275a30556c6dd089460f50eecdeeb547c
Joseph Hopson <jh@josephhopson.com>
2024-08-26 18:58:34 -0600

    init

---
+++ module/data/_module.mjs
@@ -0,0 +1,10 @@
+// Export Actors
+export { default as DarkReverieActorBase } from './base-actor.mjs';
+export { default as DarkReverieCharacter } from './actor-character.mjs';
+export { default as DarkReverieNPC } from './actor-npc.mjs';
+
+// Export Items
+export { default as DarkReverieItemBase } from './base-item.mjs';
+export { default as DarkReverieGear } from './item-gear.mjs';
+export { default as DarkReverieFeature } from './item-feature.mjs';
+export { default as DarkReverieSpell } from './item-spell.mjs';
---
+++ module/data/item-feature.mjs
@@ -0,0 +1,8 @@
+import DarkReverieItemBase from './base-item.mjs';
+
+export default class DarkReverieFeature extends DarkReverieItemBase {
+  static LOCALIZATION_PREFIXES = [
+    'DARK_REVERIE.Item.base',
+    'DARK_REVERIE.Item.Feature',
+  ];
+}
